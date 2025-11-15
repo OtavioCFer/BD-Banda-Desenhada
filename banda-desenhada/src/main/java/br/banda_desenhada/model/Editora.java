6bb808f2211a52dc419ddbaf80ecf81fa7a624fe
Otavio <otaviocfer@hotmail.com>
2025-11-15 15:31:29 -0300

    feat: 🚀 Adiciona modelo editora

---
+++ banda-desenhada/src/main/java/br/banda_desenhada/model/Editora.java
@@ -0,0 +1,43 @@
+package br.banda_desenhada.model;
+
+import java.time.OffsetDateTime;
+
+public class Editora {
+    private Integer idEditora;
+    private String nome;
+    private String pais;
+    private OffsetDateTime criadoEm;
+
+    public Integer getIdEditora() {
+        return idEditora;
+    }
+
+    public void setIdEditora(Integer idEditora) {
+        this.idEditora = idEditora;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public OffsetDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(OffsetDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }   
+
+}
