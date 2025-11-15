51bc08c6afb10b1969e781ef75e721c18fd7b2d0
Otavio <otaviocfer@hotmail.com>
2025-11-15 14:17:49 -0300

    🚀 Add modelo Quadrinho

---
+++ banda-desenhada/src/main/java/br/banda_desenhada/model/Quadrinho.java
@@ -0,0 +1,97 @@
+package br.banda_desenhada.model;
+
+import java.time.OffsetDateTime;
+
+public class Quadrinho {
+
+    private Integer idQuadrinho;
+    private String titulo;
+    private String sinopse;
+    private Integer numeroEdicao;
+    private Integer anoEdicao;
+    private Integer paginas;
+    private String isbn;
+    private String capaUrl;
+    private OffsetDateTime criadoEm;
+    private Integer idEditora;
+
+    public Integer getIdQuadrinho() {
+        return idQuadrinho;
+    }
+
+    public void setIdQuadrinho(Integer idQuadrinho) {
+        this.idQuadrinho = idQuadrinho;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public Integer getNumeroEdicao() {
+        return numeroEdicao;
+    }
+
+    public void setNumeroEdicao(Integer numeroEdicao) {
+        this.numeroEdicao = numeroEdicao;
+    }
+
+    public Integer getAnoEdicao() {
+        return anoEdicao;
+    }
+
+    public void setAnoEdicao(Integer anoEdicao) {
+        this.anoEdicao = anoEdicao;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCapaUrl() {
+        return capaUrl;
+    }
+
+    public void setCapaUrl(String capaUrl) {
+        this.capaUrl = capaUrl;
+    }
+
+    public OffsetDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(OffsetDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public Integer getIdEditora() {
+        return idEditora;
+    }
+
+    public void setIdEditora(Integer idEditora) {
+        this.idEditora = idEditora;
+    }
+}
