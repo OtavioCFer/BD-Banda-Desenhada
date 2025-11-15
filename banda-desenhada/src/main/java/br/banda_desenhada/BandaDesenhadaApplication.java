a9264f43f4d3e8504d30e288168f604ac84ce326
Otavio <otaviocfer@hotmail.com>
2025-11-14 22:11:40 -0300

    Adiciona template do Spring Boot

---
+++ banda-desenhada/src/main/java/br/banda_desenhada/BandaDesenhadaApplication.java
@@ -0,0 +1,13 @@
+package br.banda_desenhada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandaDesenhadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandaDesenhadaApplication.class, args);
+	}
+
+}
---
+++ banda-desenhada/src/test/java/br/banda_desenhada/BandaDesenhadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.banda_desenhada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandaDesenhadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
